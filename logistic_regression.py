eccb638b568e36c0b50fb944e7cee5535694cb30
Max <kolxy@qq.com>
2021-12-05 00:37:49 -0800

    log reg

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,10 +1,9 @@
-from timeit import default_timer
-
 import numpy as np
 from matplotlib import pyplot as plt
 
 import util
 from constants import DataDir
+from constants import MODE
 import os
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
@@ -13,182 +12,124 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay, f1_score, precision_score, \
     recall_score
 
-PATH_OUTPUT = os.getcwd() + "/output/"
+PATH_OUTPUT = os.getcwd() + "/output/logistic_regression/"
 
 
 def main():
-    print("Reading data")
+    util.log("Reading data")
     training = util.get_clean_dataframe_from_file(DataDir.all_tables)
     training = util.convert_input_column_type(training)
 
     print(training['attack_cat'].value_counts())
     print(dict(zip(training['attack_cat'].cat.codes, training['attack_cat'])))
 
-    # Binary PCA
-
-    # use training data for prediction
+    # Binary
+    # Raw training data
     x_train, y_train = util.get_input_output(training, class_type='binary')
-
-    _, x_train_reduced = util.reduce_features(input_data=x_train, output_data=y_train, output_data_type='binary')
-    print(x_train_reduced)
-
-    print(f'Shape of x_train: {x_train.shape}, Shape of y_train {y_train.shape}')
-
-    start_time = default_timer()
-
-    # scale the data for use with PCA
-    scaler = StandardScaler()
-    x_train = scaler.fit_transform(x_train)
-
-    # apply principal components analysis
-    pca = PCA(0.99, random_state=42)
-    x_train = pca.fit_transform(x_train)
-
-    end_time = default_timer()
-
-    print(f'Total time to run PCA (with scaling): {end_time - start_time} seconds')
-
-    print(f'Number of PCA components: {pca.n_components_}')
-
     x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.25, random_state=42)
-    run_logistic_regression(x_train, y_train, x_test, y_test, reduced=True, class_type='binary')
+    top_features = run_logistic_regression(x_train, y_train, x_test, y_test, MODE.binary)
 
-    # Multiclass PCA
-
-    # use training data for prediction
-    x_train, y_train = util.get_input_output(training, class_type='multiclass')
-
-    _, x_train_reduced = util.reduce_features(input_data=x_train,
-                                              output_data=y_train,
-                                              output_data_type='multiclass',
-                                              benign_include=True)
-    print(x_train_reduced)
-
-    print(y_train)
-
-    # scale the data for use with PCA
-    scaler = StandardScaler()
-    x_train = scaler.fit_transform(x_train)
-
-    # apply principal components analysis
-    pca = PCA(0.99, random_state=42)
-    x_train = pca.fit_transform(x_train)
-
-    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.25, random_state=42)
-    run_logistic_regression(x_train, y_train, x_test, y_test, reduced=True, class_type='multiclass')
-
-    # use training data for prediction
-    x_train, y_train = util.get_input_output(training, class_type='multiclass', benign_include=False)
-
-    _, x_train_reduced = util.reduce_features(input_data=x_train,
-                                              output_data=y_train,
-                                              output_data_type='multiclass',
-                                              benign_include=False)
-    print(x_train_reduced)
-
-    # scale the data for use with PCA
-    scaler = StandardScaler()
-    x_train = scaler.fit_transform(x_train)
-
-    # apply principal components analysis
-    pca = PCA(0.99, random_state=42)
-    x_train = pca.fit_transform(x_train)
-
-    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.25, random_state=42)
-    run_logistic_regression(x_train, y_train, x_test, y_test, reduced=True, contains_benign=False, class_type='multiclass')
-
-    # Binary No-PCA
-
-    # use training data for prediction
+    # Top features from above
     x_train, y_train = util.get_input_output(training, class_type='binary')
-
+    # x_train = x_train[[x[0] for x in top_features]]
+    _, x_train = util.reduce_features(input_data=x_train,
+                                      output_data=y_train,
+                                      output_data_type='binary',
+                                      benign_include=True)
     x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.25, random_state=42)
-    run_logistic_regression(x_train, y_train, x_test, y_test, reduced=False, class_type='binary')
+    top_features = run_logistic_regression(x_train, y_train, x_test, y_test, MODE.binary_reduced)
 
-    # Multiclass No-PCA
+    # PCA
+    x_train, y_train = util.get_input_output(training, class_type='binary')
+    scaler = StandardScaler() # scale the data for use with PCA
+    x_train = scaler.fit_transform(x_train)
+    pca = PCA(0.99, random_state=42)
+    x_train = pca.fit_transform(x_train)
+    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.25, random_state=42)
+    run_logistic_regression(x_train, y_train, x_test, y_test, MODE.binary_PCA)
 
-    # use training data for prediction
+    # Multi-class
+    # Raw training data
     x_train, y_train = util.get_input_output(training, class_type='multiclass')
-
     x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.25, random_state=42)
-    run_logistic_regression(x_train, y_train, x_test, y_test, reduced=False, class_type='multiclass')
+    top_features = run_logistic_regression(x_train, y_train, x_test, y_test, MODE.multi)
 
-    # Multiclass No-PCA
+    # Top features from above
+    x_train, y_train = util.get_input_output(training, class_type='multiclass')
+    # x_train = x_train[[x[0] for x in top_features]]
+    _, x_train = util.reduce_features(input_data=x_train,
+                                      output_data=y_train,
+                                      output_data_type='multiclass',
+                                      benign_include=True)
+    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.25, random_state=42)
+    run_logistic_regression(x_train, y_train, x_test, y_test, MODE.multi_reduced)
 
-    # use training data for prediction
+    # PCA
+    x_train, y_train = util.get_input_output(training, class_type='multiclass')
+    scaler = StandardScaler() # scale the data for use with PCA
+    x_train = scaler.fit_transform(x_train)
+    pca = PCA(0.99, random_state=42)
+    x_train = pca.fit_transform(x_train)
+    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.25, random_state=42)
+    run_logistic_regression(x_train, y_train, x_test, y_test, MODE.multi_PCA)
+
+    # No benign
+    # Raw training data WITHOUT benign labels
     x_train, y_train = util.get_input_output(training, class_type='multiclass', benign_include=False)
-
     x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.25, random_state=42)
-    run_logistic_regression(x_train, y_train, x_test, y_test, reduced=False, contains_benign=False, class_type='multiclass')
+    top_features = run_logistic_regression(x_train, y_train, x_test, y_test, MODE.no_benign)
 
-    # Multiclass Feature Selection
-
+    # Top features from above WITHOUT benign labels
     x_train, y_train = util.get_input_output(training, class_type='multiclass', benign_include=False)
-    x_train = x_train[['stcpb', 'dtcpb', 'sload']]
-
+    _, x_train = util.reduce_features(input_data=x_train,
+                                      output_data=y_train,
+                                      output_data_type='multiclass',
+                                      benign_include=False)
     x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.25, random_state=42)
+    run_logistic_regression(x_train, y_train, x_test, y_test, MODE.no_benign_reduced)
 
-    run_logistic_regression(x_train, y_train, x_test, y_test, reduced=False, contains_benign=False, class_type='multiclass')
+    # PCA WITHOUT benign labels
+    x_train, y_train = util.get_input_output(training, class_type='multiclass', benign_include=False)
+    scaler = StandardScaler()
+    x_train = scaler.fit_transform(x_train)
+    pca = PCA(0.99, random_state=42)
+    x_train = pca.fit_transform(x_train)
+    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.25, random_state=42)
+    run_logistic_regression(x_train, y_train, x_test, y_test, MODE.no_benign_PCA)
 
 
 def run_logistic_regression(x_train,
                             y_train,
                             x_test,
                             y_test,
-                            class_type='binary',
-                            contains_benign=True,
-                            reduction_type='PCA',
-                            reduced=False):
-    clf = LogisticRegression(multi_class='multinomial',
-                             solver='saga',
-                             max_iter=1500,
-                             n_jobs=-1,
-                             random_state=42)
-    print(f"Fitting - reduced? {reduced} - class type? {class_type} - benign? {contains_benign}")
-    start_time = default_timer()
+                            mode):
+    clf = LogisticRegression(max_iter = 1500)
+    util.log("=============================Fitting=============================")
+    util.log(f"Current type: {mode.value}")
     clf.fit(x_train, y_train)
-    end_time = default_timer()
-    print(f'Total time to fit: {end_time - start_time} seconds')
-    print(f"Predicting - reduced? {reduced} - class type? {class_type} - benign? {contains_benign}")
-    start_time = default_timer()
+    util.log(f"Predicting")
     predict = clf.predict(x_test)
-    end_time = default_timer()
-    print("Generating confusion matrix")
-    print(f'Total time to predict: {end_time - start_time} seconds')
+
+    # Confusion matrix
+    util.log("Generating confusion matrix")
     cm = confusion_matrix(y_test, predict)
     display_cm = ConfusionMatrixDisplay(confusion_matrix=cm)
     display_cm.plot()
-    if class_type == 'binary':
-        if reduced:
-            plt.title(f'Logistic Regression {reduction_type} {class_type} Confusion Matrix')
-            plt.savefig(PATH_OUTPUT + f'random_forest_{reduction_type}_{class_type}_confusion_matrix.png')
-        else:
-            plt.title(f'Logistic Regression no PCA or FAMD {class_type} Confusion Matrix')
-            plt.savefig(PATH_OUTPUT + f'random_forest_no_pca_or_famd_{class_type}_confusion_matrix.png')
-    else:
-        if contains_benign:
-            if reduced:
-                plt.title(f'Logistic Regression {reduction_type} {class_type} Confusion Matrix')
-                plt.savefig(PATH_OUTPUT + f'random_forest_{reduction_type}_{class_type}_confusion_matrix.png')
-            else:
-                plt.title(f'Logistic Regression no PCA or FAMD {class_type} Confusion Matrix')
-                plt.savefig(PATH_OUTPUT + f'random_forest_no_pca_or_famd_{class_type}_confusion_matrix.png')
-        else:
-            if reduced:
-                plt.title(f'Logistic Regression {reduction_type} {class_type} Confusion Matrix - No Benign')
-                plt.savefig(PATH_OUTPUT + f'random_forest_pca_{class_type}_confusion_matrix_no_benign.png')
-            else:
-                plt.title(f'Logistic Regression no PCA or FAMD {class_type} Confusion Matrix - No Benign')
-                plt.savefig(PATH_OUTPUT + f'logistic_regression_no_pca_or_famd_{class_type}_confusion_matrix_no_benign.png')
-
+    plt.title(f'Logistic Regression {mode.value} Confusion Matrix')
+    plt.savefig(PATH_OUTPUT + f'Logistic Regression Confusion Matrix - {mode.value}.png')
     plt.close()
-    print(f"Accuracy - {class_type}: {accuracy_score(y_test, predict)}")
-    print(f"F1 Score - {class_type}: {f1_score(y_test, predict, average='micro')}")
-    print(f"Precision - {class_type}: {precision_score(y_test, predict, average='micro')}")
-    print(f"Recall - {class_type}: {recall_score(y_test, predict, average='micro')}")
 
-    if not reduced:
-        print("Generating graph")
+    # scores
+    print(f"Accuracy - {mode.value}: {accuracy_score(y_test, predict)}")
+    # These scores only available to binary
+    if MODE.is_binary(mode):
+        print(f"F1 Score - {mode.value}: {f1_score(y_test, predict)}")
+        print(f"Precision - {mode.value}: {precision_score(y_test, predict)}")
+        print(f"Recall - {mode.value}: {recall_score(y_test, predict)}")
+
+    # feature importance
+    if MODE.is_raw(mode):
+        util.log("Generating graph")
 
         # create importance dictionary as {name: importance}
         feature_dict = dict(zip(x_train.columns, np.std(x_train, 0) * clf.coef_[0]))
@@ -200,31 +141,20 @@ def run_logistic_regression(x_train,
         top_values = [x[1] for x in top]
 
         # output graph
-        plt.close()
         plt.barh(top_names, top_values)
         plt.gca().invert_yaxis()
-
-        if contains_benign:
-            plt.title(f"Logistic regression feature importance - {class_type}")
-        else:
-            plt.title(f"Logistic regression feature importance - no benign - {class_type}")
+        plt.title(f"Logistic regression feature importance {mode.value}")
 
         plt.xlabel("Importance")
         plt.ylabel("Feature name")
         for index, value in enumerate(top_values):
             plt.text(value, index, str("{:.2e}".format(float(value))))
 
-        if contains_benign:
-            plt.savefig(PATH_OUTPUT + f"Logistic Regression feature importance - {class_type}.png")
-            print("Output: ")
-            print(PATH_OUTPUT + f"Logistic Regression feature importance - {class_type}.png")
-        else:
-            plt.savefig(PATH_OUTPUT + f"Logistic Regression feature importance - no benign - {class_type}.png")
-            print("Output: ")
-            print(PATH_OUTPUT + f"Logistic Regression feature importance - no benign - {class_type}.png")
+        plt.savefig(PATH_OUTPUT + f"Logistic Regression feature importance - {mode.value}.png")
+        util.log("Output: ")
+        print(PATH_OUTPUT + f"Logistic Regression feature importance - {mode.value}.png")
 
         plt.close()
-
         return top
 
 
